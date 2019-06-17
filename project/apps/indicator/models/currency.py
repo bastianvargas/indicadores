985cf4bce56ddb136db34241cd9e0fb0a6f8416e
Bastian <bastianlc@live.cl>
2019-06-16 21:12:57 -0400

    add fields date and value in model currency

--- project/apps/indicator/models/currency.py
+++ project/apps/indicator/models/currency.py
@@ -5,6 +5,8 @@ from django.db import models
 class Currency(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     code = models.CharField(max_length=255)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    value = models.FloatField(default=None)
 
     def __str__(self):
         return self.code
