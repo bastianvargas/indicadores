a3dcc6897eb73ae9c17fa8d4ffd2d51ab97ff416
Bastian <bastianlc@live.cl>
2019-06-18 02:59:01 -0400

    modified models, new table date

--- project/apps/indicator/models/currency.py
+++ project/apps/indicator/models/currency.py
@@ -5,11 +5,19 @@ from django.db import models
 class Currency(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     code = models.CharField(max_length=255)
-    date = models.DateField(auto_now=False, auto_now_add=False)
-    value = models.FloatField(default=None)
+
 
     def __str__(self):
         return self.code
 
     class Meta:
         verbose_name_plural = 'currencies'
+
+class Date(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    currency = models.ForeignKey('Currency',on_delete=models.CASCADE)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    value = models.FloatField(default=None)
+
+    def __str__(self):
+        return self.date
